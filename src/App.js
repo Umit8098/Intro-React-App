60df4ecde4e854351d81931dea3beeae6f3c0d08
Ümit Arat <umitarat8098@gmail.com>
2025-02-01 23:16:21 +0300

    Create React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+//! Component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //! JSX -> HTML'in JS'ye gömülmüş halidir.
+    <div>
+      <h1>My first React App</h1>
     </div>
   );
 }
 
 export default App;
+// The App component is a function component that returns JSX.
